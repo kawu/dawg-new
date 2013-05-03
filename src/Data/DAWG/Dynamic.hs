cc9c3b6775972d8f8ea68378867eb4f1979ed9ae
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2013-05-03 14:44:18 +0200

    Current snapshot

--- src/Data/DAWG/Dynamic.hs
+++ src/Data/DAWG/Dynamic.hs
@@ -1,9 +1,36 @@
+-- | The module implements /directed acyclic word graphs/ (DAWGs) internaly
+-- represented as /minimal acyclic deterministic finite-state automata/.
+-- The implementation provides fast insert and delete operations
+-- which can be used to build the DAWG structure incrementaly.
+
+
 module Data.DAWG.Dynamic
-( DFA (..)
-, empty
-, insert
-, lookup
-, printDFA
+(
+-- * DAWG type
+  DAWG
+
+-- -- * Query
+-- , numStates
+-- , numEdges
+-- , lookup
+-- 
+-- -- * Construction
+-- , empty
+-- , fromList
+-- , fromListWith
+-- , fromLang
+-- 
+-- -- ** Insertion
+-- , insert
+-- , insertWith
+-- 
+-- -- ** Deletion
+-- , delete
+-- 
+-- -- * Conversion
+-- , assocs
+-- , keys
+-- , elems
 ) where
 
 
@@ -14,29 +41,105 @@ import           Data.DAWG.Dynamic.Types
 import qualified Data.DAWG.Dynamic.Internal as I
 
 
--- A Minimal, acyclic, deterministic, finite-state automaton.
-data DFA s = DFA
+-- | Empty DAWG.
+data DAWG s = DAWG
     { body  :: I.DFA s
     , root  :: StateID }
 
 
--- | An empty DFA.
-empty :: ST s (DFA s)
+-- | An empty DAWG.
+empty :: ST s (DAWG s)
 empty = do
     (dfa, i) <- I.empty
-    return $ DFA dfa i
+    return $ DAWG dfa i
 
 
--- | Insert a (word, value) pair into the DFA.
-insert :: DFA s -> [Sym] -> Val -> ST s ()
-insert dfa xs y = I.insertRoot (body dfa) xs y (root dfa)
-
-
--- | Lookup a word in the automaton.
-lookup :: DFA s -> [Sym] -> ST s (Maybe Val)
-lookup dfa xs = I.lookup (body dfa) xs (root dfa)
-
-
--- | A helper DFA printing function.
-printDFA :: DFA RealWorld -> IO ()
-printDFA dfa = I.printDFA $ body dfa
+-- -- | Number of states in the automaton.
+-- numStates :: DAWG a b -> Int
+-- numStates = G.size . graph
+-- 
+-- -- | Number of edges in the automaton.
+-- numEdges :: DAWG a b -> Int
+-- numEdges = sum . map (length . N.edges) . G.nodes . graph
+-- 
+-- -- | Insert the (key, value) pair into the DAWG.
+-- insert :: (Enum a, Ord b) => [a] -> b -> DAWG a b -> DAWG a b
+-- insert xs' y d =
+--     let xs = map fromEnum xs'
+--         (i, g) = S.runState (insertM xs y $ root d) (graph d)
+--     in  DAWG g i
+-- {-# INLINE insert #-}
+-- {-# SPECIALIZE insert :: Ord b => String -> b -> DAWG Char b -> DAWG Char b #-}
+-- 
+-- -- | Insert with a function, combining new value and old value.
+-- -- 'insertWith' f key value d will insert the pair (key, value) into d if
+-- -- key does not exist in the DAWG. If the key does exist, the function
+-- -- will insert the pair (key, f new_value old_value).
+-- insertWith
+--     :: (Enum a, Ord b) => (b -> b -> b)
+--     -> [a] -> b -> DAWG a b -> DAWG a b
+-- insertWith f xs' y d =
+--     let xs = map fromEnum xs'
+--         (i, g) = S.runState (insertWithM f xs y $ root d) (graph d)
+--     in  DAWG g i
+-- {-# SPECIALIZE insertWith
+--         :: Ord b => (b -> b -> b) -> String -> b
+--         -> DAWG Char b -> DAWG Char b #-}
+-- 
+-- -- | Delete the key from the DAWG.
+-- delete :: (Enum a, Ord b) => [a] -> DAWG a b -> DAWG a b
+-- delete xs' d =
+--     let xs = map fromEnum xs'
+--         (i, g) = S.runState (deleteM xs $ root d) (graph d)
+--     in  DAWG g i
+-- {-# SPECIALIZE delete :: Ord b => String -> DAWG Char b -> DAWG Char b #-}
+-- 
+-- -- | Find value associated with the key.
+-- lookup :: (Enum a, Ord b) => [a] -> DAWG a b -> Maybe b
+-- lookup xs' d =
+--     let xs = map fromEnum xs'
+--     in  S.evalState (lookupM xs $ root d) (graph d)
+-- {-# SPECIALIZE lookup :: Ord b => String -> DAWG Char b -> Maybe b #-}
+-- 
+-- -- | Return all key/value pairs in the DAWG in ascending key order.
+-- assocs :: (Enum a, Ord b) => DAWG a b -> [([a], b)]
+-- assocs
+--     = map (first (map toEnum))
+--     . (assocsAcc <$> graph <*> root)
+-- {-# SPECIALIZE assocs :: Ord b => DAWG Char b -> [(String, b)] #-}
+-- 
+-- -- | Return all keys of the DAWG in ascending order.
+-- keys :: (Enum a, Ord b) => DAWG a b -> [[a]]
+-- keys = map fst . assocs
+-- {-# SPECIALIZE keys :: Ord b => DAWG Char b -> [String] #-}
+-- 
+-- -- | Return all elements of the DAWG in the ascending order of their keys.
+-- elems :: Ord b => DAWG a b -> [b]
+-- elems = map snd . (assocsAcc <$> graph <*> root)
+-- 
+-- -- | Construct DAWG from the list of (word, value) pairs.
+-- fromList :: (Enum a, Ord b) => [([a], b)] -> DAWG a b
+-- fromList xs =
+--     let update t (x, v) = insert x v t
+--     in  foldl' update empty xs
+-- {-# INLINE fromList #-}
+-- {-# SPECIALIZE fromList :: Ord b => [(String, b)] -> DAWG Char b #-}
+-- 
+-- -- | Construct DAWG from the list of (word, value) pairs
+-- -- with a combining function.  The combining function is
+-- -- applied strictly.
+-- fromListWith
+--     :: (Enum a, Ord b) => (b -> b -> b)
+--     -> [([a], b)] -> DAWG a b
+-- fromListWith f xs =
+--     let update t (x, v) = insertWith f x v t
+--     in  foldl' update empty xs
+-- {-# SPECIALIZE fromListWith
+--         :: Ord b => (b -> b -> b)
+--         -> [(String, b)] -> DAWG Char b #-}
+-- 
+-- -- | Make DAWG from the list of words.  Annotate each word with
+-- -- the @()@ value.
+-- fromLang :: Enum a => [[a]] -> DAWG a ()
+-- fromLang xs = fromList [(x, ()) | x <- xs]
+-- {-# SPECIALIZE fromLang :: [String] -> DAWG Char () #-}
--- src/Data/DAWG/Dynamic/Internal.hs
+++ src/Data/DAWG/Dynamic/Internal.hs
@@ -2,119 +2,8 @@
 {-# LANGUAGE RecordWildCards #-}
 
 
-{-
- - Dodanie nowego słowa do słownika:
- -
- -
- - DEFINICJA: Stan jest konfluentny wtw. gdy liczba wchodzących
- - do niego ścieżek jest większa niż 1 (alternatywnie, gdy jego
- - lewy język ma rozmiar większy niż 1).
- - I NIE MA TUTAJ ZNACZENIA LICZBA WCHODZĄCYCH KRAWEDZI!
- -
- -
- - FAKT: Stan klonujemy wtw stan jest konfluentny.
- -
- - 
- - a) Stan niekonfluentny
- - 
- - Co robimy, gdy stan nie jest konfluentny?
- - Stan ten ma jedną ścieżkę wchodzącą.
- - Nie kopiujemy go.  Teoretycznie, powinniśmy usunąć
- - oryginalny stan i dodać zmodyfikowany, ale ponieważ
- - oryginalny stan nie będzie już potrzebny, możemy
- - wykorzystać go do przechowania informacji nowego
- - stanu.  Jeśli chodzi o krawędzie prowadzące do
- - oryginalnego stanu, jest tylko jedna taka krawędź,
- - po której i tak przechodzimy w ramach dodawania
- - nowego wpisu do słownika.
- -
- - Co robić, gdy zmodyfikowany stan jest już
- - elementem automatu?
- - * Modyfikujemy oryginalny stan "w miejscu"
- - * Wyszukujemy go w automacie
- - * Jeśli nie ma, to koniec obliczeń
- - * Jeśli jest, "usuwamy" zmodyfikowany stan i
- -   przekierowujemy krawędzie które wchodziły do
- -   zmodyfikowanego stanu tak, żeby wchodziły do
- -   izomorficznego stanu znalezionego w automacie
- -   (no problem).
- -
- - Zgodnie z opisaną wyżej procedurą w niektórych
- - przypadkach będziemy usuwali stany automatu.
- - Co będziemy robić z takimi stanami?  Możemy je
- - przechowywać na "stosie stanów usuniętych".
- - Czy raczej na stosie "identyfikatorów stanów
- - usuniętych", dzięki któremu będziemy mogli
- - szybko dodawać nowe stany do automatu. 
- - 
- -
- -
- - b) Stan konfluentny
- -
- - Co w sytuacji, gdy stan jest konfluentny?
- - Jedna "instancja" oryginalnego stanu ma zostać
- - usunięta, natomiast pozostałe -- nie.
- - Tak więc powinniśmy utworzyć nowe "pole", w którym
- - zapisany zostanie stan zmodyfikowany.  Oryginalny stan
- - nie zostanie usunięty, jest wiele ścieżek do niego
- - wchodzących. 
- -
- - ALE: Jest możliwe, że zmodyfikowany stan jest już
- - elementem automatu.
- - TEZA: W takiej sytuacji nie ma sensu tworzyć "pola"
- - na nowy stan, ponieważ pole to i tak nie zostanie
- - wykorzystane.  Stan zmodyfikowany zostanie skopiowany
- - do nowego pola, a potem okaże się że zmodyfikowany
- - stan jest już w automacie i nowe pole zostanie
- - usunięte.
- -
- - POMYSŁ: Zamiast kopiować stan do nowego pola, możemy
- - zmodyfikować aktualny stan i sprawdzić czy taki stan
- - znajduje się w automacie.  Przez ten krótki czas:
- - * Nie będzie zgodności pomiędzy stanem a tablicą haszów.
- - * Zawartość stanu będzie nieprawidłowa.
- -
- - Informacje o nowym stanie powinny być dodawane do tablicy
- - haszów tylko wtedy, gdy do automatu dodawany jest rzeczywiście
- - nowy stan.  Powinniśmy podzielić funkcje ze względu na to,
- - czu zachowują spójność pomiędzy tablicą stanów oraz tablicą
- - haszów.
- -
- -
- - Stan aplikacji
- - ==============
- - 
- - Elementy stanu aplikacji:
- -
- - * Tablica (wektor) stanów automatu.  Do każdego stanu
- -   przypisane są wartość oraz zbiór krawędzi wychodzących.
- -   Przez identifykator stanu będziemy rozumieć pozycję
- -   tego stanu w tablicy stanów automatu. 
- -
- - * Tablica asocjacyjna, która pozwala przekształcać stany
- -   (a dokładniej, ich hasze) na identyfikatory stanów.
- -   Warto zauważyć, że operację odwrotną mamy w pewnym
- -   sensie za darmo: znając identyfikator stanu, możemy
- -   szybko wyszukać odpowiedni stan w tablicy stanów automatu,
- -   na podstawie którego możemy już obliczeń wartość hasz.
- -
- -
- - Notatki
- - =======
- -
- - Na etapie obliczeń musimy aktualizować (m.in.) dwa ważne elementy
- - stanu: liczby wchodzących do poszczególnych stanów ścieżek oraz 
- - mapę haszów.
- -
- - Tablica stanów dzieli się na dwie grupy: stany, które należą do
- - automatu, oraz stany "nieaktywne".  Należy wyszczególnić, które
- - funkcje działają na stanach aktywnych, a które na nieaktywnych.
- -
- -}
-
-
 module Data.DAWG.Dynamic.Internal
-( DFA_State (..)
+( DFAData (..)
 , DFA
 , empty
 , insert
@@ -129,6 +18,7 @@ import           Control.Applicative ((<$>), (<$), (<*>), pure)
 import           Control.Monad (void, forM_)
 import           Data.Int (Int32)
 import           Data.Maybe (fromJust)
+import qualified Data.Traversable as T
 import qualified Data.Vector.Mutable as V
 import qualified Data.Vector.Unboxed.Mutable as U
 import qualified Data.Map as M
@@ -146,62 +36,58 @@ import qualified Data.DAWG.Dynamic.Stack as P
 ----------------------------------------------------------------------
 
 
--- | A state of DFA application (*not* a state of an automaton!).
-data DFA_State s = DFA_State {
-    -- | A vector of DFA states.  A position of a state
-    -- in the vector represents its `StateID`.
+-- | DFA consists of a set of states and transitions between individual states.
+-- Each state may be active (reachable from the root state) or inactive
+-- (otherwise).  DFA data can be used within the context of the `ST` monad
+-- (hence the @s@ parameter).
+--
+-- TODO: This representation is a bit inconsistent.  Some fields are
+-- represented directly (`stateVect`, `ingoVect`), other as pointers
+-- (`freeStack`, `stateMap`).  Perhaps all fields should be stored
+-- as pointers?  Then we could get rid of the `DFADAta`/`DFA`
+-- distinction.
+data DFAData s = DFAData {
+    -- | A vector of DFA states (both active and inactive).  A position of
+    -- a state in the vector represents its `StateID`.
       stateVect :: V.MVector s State
     -- | A number of ingoing paths (size of the left language)
-    -- for each state in the automaton.
+    -- for each active state in the automaton.
     , ingoVect  :: U.MVector s Int32
-    -- | A stack of free state slots.
+    -- | A stack of free state slots, i.e. inactive states identifiers.
     , freeStack :: P.Stack s StateID
-    -- | A hash table which is used to translate states
-    -- to their corresponding identifiers.
+    -- | A map which is used to translate active states to their
+    -- corresponding identifiers.  Inactive states are not kept
+    -- it the map.
+    -- TODO: The root state can be treated in a special way.  In particular,
+    -- it should not be an element of the `stateMap`.
     , stateMap  :: M.Map State StateID }
 
 
 -- | A DFA reference.
-type DFA s = STRef s (DFA_State s)
-
-
--- | An empty DFA with one root state ID.
-empty :: ST s (DFA s, StateID)
-empty = do
-    dfaState <- DFA_State
-        <$> V.new 0
-        <*> U.new 0
-        <*> P.empty
-        <*> pure M.empty
-    dfa  <- newSTRef dfaState
-    i    <- _addState dfa N.empty
-    return (dfa, i)
+type DFA s = STRef s (DFAData s)
 
 
 ----------------------------------------------------------------------
--- Low-level operations
+-- Low-level operations (work on both active and inactive states).
 ----------------------------------------------------------------------
 
 
--- | Grow DFA with new, empty states.
+-- | Grow DFA with new inactive states.
 grow :: DFA s -> ST s ()
 grow dfa = do
-    dfaState@DFA_State{..} <- readSTRef dfa
+    dfaData@DFAData{..} <- readSTRef dfa
     let n = V.length stateVect
 
     -- Use n+1 to handle empty dfa.
     stateVect' <- V.grow stateVect (n + 1)
     ingoVect'  <- U.grow ingoVect  (n + 1)
-    writeSTRef dfa $ dfaState
+    writeSTRef dfa $ dfaData
         { stateVect = stateVect'
         , ingoVect  = ingoVect' }
 
-    forM_ [n .. 2*n] $ \i -> do
+    let n2 = n*2
+    forM_ [n2, n2-1 .. n] $ \i -> do
         P.push i freeStack
-        -- TODO: Check, if it is not slower when
-        -- using uninitialized values.
-        -- setState dfa i N.empty
-        -- setI dfa i 0
 
 
 -- | Retrieve state with a given identifier.
@@ -230,10 +116,36 @@ setIngo :: DFA s -> StateID -> Int -> ST s ()
 setIngo dfa i x = do
     v <- ingoVect <$> readSTRef dfa
     U.write v i (fromIntegral x)
+-- | Lookup state identifier in a DFA.
+lookupState :: DFA s -> State -> ST s (Maybe StateID)
+lookupState dfa u = M.lookup u . stateMap <$> readSTRef dfa
+
+
+-- | Add new state into the automaton.  The function assumes,
+-- that the state is not a member of the automaton.
+_addState :: DFA s -> State -> ST s StateID
+_addState dfa u = do
+    free <- freeStack <$> readSTRef dfa
+    i <- P.pop free >>= \case
+        Just i  -> return i
+        Nothing -> do
+            grow dfa
+            fromJust <$> P.pop free
+    setState dfa i u
+    setIngo  dfa i 1
+    modifySTRef dfa $ \dfaData ->
+        let stateMap' = M.insert u i (stateMap dfaData)
+        in  dfaData { stateMap = stateMap' }
+    return i
+
+
+----------------------------------------------------------------------
+-- Medium-level interface (an additional layer between the low-level
+-- and the high-level functionality).
+----------------------------------------------------------------------
 
 
 -- | Incerement the number of ingoing paths for a given state.
--- TODO: Implement and use modifyIngo function?
 incIngo :: DFA s -> StateID -> ST s ()
 incIngo dfa i = do
     v <- ingoVect <$> readSTRef dfa
@@ -242,7 +154,6 @@ incIngo dfa i = do
 
 
 -- | Incerement the number of ingoing paths for a given state.
--- TODO: Implement and use modifyIngo function?
 decIngo :: DFA s -> StateID -> ST s ()
 decIngo dfa i = do
     v <- ingoVect <$> readSTRef dfa
@@ -258,7 +169,6 @@ getTrans dfa i x = N.getTrans x <$> getState dfa i
 
 -- | Set the outgoing transition.  Use `Nothing` to delete
 -- the transition.
--- TODO: Implement modifyTrans and use it here.
 setTrans :: DFA s -> StateID -> Sym -> Maybe StateID -> ST s ()
 setTrans dfa i x j = do
     u <- getState dfa i
@@ -271,77 +181,46 @@ getValue dfa i = N.value <$> getState dfa i
 
 
 -- | Set a state value.
--- TODO: Implement modifyValue and use it here.
 setValue :: DFA s -> StateID -> Maybe Val -> ST s ()
 setValue dfa i y = do
     u <- getState dfa i
     setState dfa i $ N.setValue y u
 
 
--- | Lookup state identifier in a DFA.
-lookupState :: DFA s -> State -> ST s (Maybe StateID)
-lookupState dfa u = M.lookup u . stateMap <$> readSTRef dfa
-
-
--- -- | Same as `lookupState`, but looks for a state equivalent to a state residing
--- -- under a given identifier.  Intuitively, it should be the same state ID
--- -- as the one supplied as argument.  However, if the state is not yet
--- -- registered in the hash table `stateMap`, the function will return
--- -- another equivalent state in the automaton, if present, and this is
--- -- the functionality we use in other, higher-level functions.
--- lookupStateID :: DFA s -> StateID -> ST s (Maybe StateID)
--- lookupStateID dfa i = getState dfa i >>= lookupState dfa
-
-
--- | Add new state into the automaton.  The function assumes,
--- that the state is not a member of the automaton.
-_addState :: DFA s -> State -> ST s StateID
-_addState dfa u = do
-    free <- freeStack <$> readSTRef dfa
-    i <- P.pop free >>= \case
-        Just i  -> return i
-        Nothing -> do
-            grow dfa
-            fromJust <$> P.pop free
-    setState dfa i u
-    setIngo  dfa i 1
-    modifySTRef dfa $ \dfaState ->
-        let stateMap' = M.insert u i (stateMap dfaState)
-        in  dfaState { stateMap = stateMap' }
-    return i
-
-
--- | Add new state into the automaton.  First check, if it
--- is not already a member of the automaton.
+-- | Add new active state into the automaton.  If its already a member of
+-- the automaton, just increase the number of ingoing paths.  Otherwise,
+-- one of the inactive states will be used.
 --
--- We assume, that every time a new state is added, it is added
--- within a context of a new path.  Therefore, if the state is
--- already a member of the automaton, we increase the number of
--- ingoing paths to this state.
+-- TODO: Rename to addState' or something similar.
 addState :: DFA s -> State -> ST s StateID
 addState dfa u = lookupState dfa u >>= \case
     Nothing -> _addState dfa u
     Just i  -> i <$ incIngo dfa i
 
 
--- | Add new state into the automaton.  First check, if it
--- is not already a member of the automaton.
+-- | Add new active state into the automaton.  If its already a member of
+-- the automaton, just increase the number of ingoing paths.  Otherwise,
+-- one of the inactive states will be used.
 --
--- We assume, that every time a new state is added, it is added
--- within a context of a new path.  Therefore, if the state is
--- already a member of the automaton, we increase the number of
--- ingoing paths to this state.
+-- TODO: Rename to addState.
+-- TODO: Maybe we should check here, it the state is useful?
+--
+-- The function satisfies the following law:
+-- (removeState dfa i >> addStateID dfa i) == return i
 addStateID :: DFA s -> StateID -> ST s StateID
 addStateID dfa i = getState dfa i >>= addState dfa
 
 
 -- | Remove state from the automaton.
+--
+-- The function satisfies the following law:
+-- (removeState dfa i >> addStateID dfa i) == return i
 removeState :: DFA s -> StateID -> ST s ()
 removeState dfa i = do
-    dfaState@DFA_State{..} <- readSTRef dfa
+    dfaData@DFAData{..} <- readSTRef dfa
     P.push i freeStack
     stateMap' <- flip M.delete stateMap <$> getState dfa i
-    writeSTRef dfa $ dfaState { stateMap = stateMap' }
+    writeSTRef dfa $ dfaData { stateMap = stateMap' }
 
 
 -- | Create a new branch in a DFA.
@@ -353,30 +232,26 @@ branch dfa [] y = do
     addState dfa $ N.state (Just y) []
 
 
--- -- | Copy state to a new slot and set the number of
--- -- ingoing paths to 1 in the new slot.
--- copy :: DFA s -> StateID -> ST s StateID
--- copy dfa i = do
---     _addState dfa =<< getState dfa i
-
-
 ----------------------------------------------------------------------
--- Medium-level interface
+-- High-level functionality
 ----------------------------------------------------------------------
 
 
--- TODO: The functions below will probably "crash" when the element
--- beeing inserted is already element of the DFA.  Or if we change
--- value of existing state.  Check it.
---
--- Analysis:
--- * Lookup is working, which suggests that state*, ingo* and free*
---   components are correct.
--- * printDFA doen't work -- contents of stateMap are incorrect!
+-- | An empty DFA with one root state ID.
+empty :: ST s (DFA s, StateID)
+empty = do
+    dfaData <- DFAData
+        <$> V.new 0
+        <*> U.new 0
+        <*> P.empty
+        <*> pure M.empty
+    dfa  <- newSTRef dfaData
+    i    <- addState dfa N.empty
+    return (dfa, i)
 
 
 -- | Insert a (word, value) pair within a context of a state.
-insert :: DFA s -> [Sym] -> Val -> StateID -> ST s StateID
+insert :: DFA s -> [Sym] -> Maybe Val -> StateID -> ST s (Maybe StateID)
 insert dfa xs y i = do
     ingoNum <- getIngo dfa i
     let doInsert = if ingoNum > 1
@@ -385,31 +260,41 @@ insert dfa xs y i = do
     doInsert dfa xs y i
 
 
+-- | Compute `Just` value when `True` or `Nothing` otherwise.
+whenTrue :: Monad m => Bool -> m a -> m (Just a)
+whenTrue x m = if x
+    then Just <$> m
+    else return Nothing
+
+
 -- | Insert a (word, value) pair within a context of a confluent state.
 -- TODO: Cut off computation when (j0 == j1).
-insertConfl :: DFA s -> [Sym] -> Val -> StateID -> ST s StateID
+insertConfl :: DFA s -> [Sym] -> Maybe Val -> StateID -> ST s (Maybe StateID)
 
 
 -- CASE: Non-empty path.
 -- TODO: Cut off computation when j0 == j1.
 insertConfl dfa (x:xs) y i0 = do
     j0 <- getTrans dfa i0 x
-    j1 <- Just <$> case j0 of
+    j1 <- case j0 of
         Just j  -> insertConfl dfa xs y j
-        Nothing -> branch dfa xs y
-    setTrans dfa i0 x j1
-    i1 <- addStateID dfa i0
-    setTrans dfa i0 x j0
-    decIngo dfa i0
-    return i1
+        Nothing -> T.mapM (branch dfa xs) y
+    if j0 == j1 then do
+        return Nothing
+    else do
+        setTrans dfa i0 x j1
+        i1 <- usefulState dfa i0 `whenTrue` addStateID dfa i0
+        setTrans dfa i0 x j0
+        decIngo dfa i0
+        return i1
 
 
 -- CASE: Empty path.
 -- TODO: Cut off computation when y0 == y1.
 insertConfl dfa [] y1 i0 = do
     y0 <- getValue dfa i0
-    setValue dfa i0 $ Just y1
-    i1 <- addStateID dfa i0
+    setValue dfa i0 y1
+    i1 <- usefulState dfa i0 `whenTrue` addStateID dfa i0
     setValue dfa i0 y0
     decIngo dfa i0
     return i1
@@ -420,15 +305,17 @@ insertNonConfl :: DFA s -> [Sym] -> Val -> StateID -> ST s StateID
 
 
 -- CASE: Non-empty path.
--- TODO: Cut off computation when j0 == j1.
 insertNonConfl dfa (x:xs) y i0 = do
     j0 <- getTrans dfa i0 x
-    j1 <- Just <$> case j0 of
+    j1 <- case j0 of
         Just j  -> insert dfa xs y j
-        Nothing -> branch dfa xs y
-    removeState dfa i0
-    setTrans dfa i0 x j1
-    addStateID dfa i0
+        Nothing -> T.mapM (branch dfa xs) y
+    if j0 == j1 then do
+        return Nothing
+    else do
+        removeState dfa i0
+        setTrans dfa i0 x j1
+        usefulState dfa i0 `whenTrue` addStateID dfa i0
 
 
 -- CASE: Empty path.
@@ -436,22 +323,22 @@ insertNonConfl dfa (x:xs) y i0 = do
 insertNonConfl dfa [] y1 i0 = do
     _y0 <- getValue dfa i0
     removeState dfa i0
-    setValue dfa i0 $ Just y1
-    addStateID dfa i0
+    setValue dfa i0 y1
+    usefulState dfa i0 `whenTrue` addStateID dfa i0
 
 
 -- | Insert a (word, value) pair under the assumption, that
 -- a state ID represents a DFA root.  In particular, ID
 -- of the root doen't change.
+-- TODO: Rename this function to insert?
 insertRoot :: DFA s -> [Sym] -> Val -> StateID -> ST s ()
-insertRoot dfa xs y i = void $ insertNonConfl dfa xs y i
--- insertRoot dfa [] y i = setValue dfa i $ Just y
--- insertRoot dfa (x:xs) y i = do
---     j0 <- getTrans dfa i x
---     j1 <- Just <$> case j0 of
---         Just j  -> insert dfa xs y j
---         Nothing -> branch dfa xs y
---     setTrans dfa i x j1
+insertRoot dfa (x:xs) y i0 = do
+    j0 <- getTrans dfa i0 x
+    j1 <- case j0 of
+        Just j  -> insert dfa xs y j
+        Nothing -> T.mapM (branch dfa xs) y
+    unless (j0 == j1) $ setTrans dfa i0 x j1
+insertRoot dfa [] y1 i0 = setValue dfa i0 y1
 
 
 -- | Lookup a word in a DFA.
@@ -471,7 +358,7 @@ lookup dfa [] i     = getValue dfa i
 printDFA :: DFA RealWorld -> IO ()
 printDFA dfa = do
     -- TODO: Print size of the left-language
-    DFA_State{..} <- stToIO $ readSTRef dfa
+    DFAData{..} <- stToIO $ readSTRef dfa
     forM_ (M.toList stateMap) $ \(state, i) -> do
         putStrLn $ "== " ++ show i ++ " =="
         N.printState state
